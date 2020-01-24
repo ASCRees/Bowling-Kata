b98efae194dafc1aaddf0a711b0f8c92ff3a01f7
arees2020 <andrew.rees2020@gmail.com>
2020-01-24 21:25:02 +0000

    feature/Bowling-04 - Added further unit tests for Bowl

---
+++ BowlingScoring/BowlingScoring.UnitTests/BowlTests.cs
@@ -0,0 +1,40 @@
+﻿namespace BowlingScoring.UnitTests
+{
+    using BowlingScoring;
+    using BowlingScoring.Interfaces;
+    using FluentAssertions;
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class BowlTests
+    {
+        private IBowl bowlball;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            //Arrange
+            bowlball = new Bowl();
+        }
+
+        [Test]
+        public void Check_If_Bowl_Exists()
+        {
+            //Act
+            Type bowlType = bowlball.GetType();
+            //Assert
+            Assert.IsNotNull(bowlType);
+        }
+
+        [Test]
+        public void Check_Bowl_Values()
+        {
+            //Act
+            Type bowlType = bowlball.GetType();
+            //Assert
+            bowlball.BowlBall(10).Should().BeInRange(0, 10, "because it cannot be outside the range 0-10");
+        }
+    }
+}
